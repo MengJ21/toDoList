bc1fd8a0c03c57ca36ee817d670bb2b384ba44ec
周孟举 <1747153981@qq.com>
2023-04-18 22:28:21 +0800

    feat: 加入redis缓存当日的任务列表。

---
+++ src/main/java/com/meng/todolist/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.meng.todolist.config;/**
+* @description TODO
+* @author 梦举
+* @date 2023/4/18 21:09
+* @version 1.0
+*/
+    
+public class RedisConfig {
+}
---
+++ src/main/java/com/meng/todolist/utils/RedisConstants.java
@@ -0,0 +1,9 @@
+package com.meng.todolist.utils;/**
+* @description TODO
+* @author 梦举
+* @date 2023/4/18 21:56
+* @version 1.0
+*/
+    
+public class RedisConstants {
+}
